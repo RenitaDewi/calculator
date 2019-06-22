f2cc65d0a4d16bbbe28f8cb8fd678af3c0b8b6d4
Rere <renitadewi55@gmail.com>
2019-06-22 23:12:29 +0700

    init js

--- FirstEd/calc.js
+++ FirstEd/calc.js
@@ -2,3 +2,7 @@ let hasil, input1, input2, operator, btnhasil, btnreset;
 
 input1 = document.querySelectorAll("input")[1];
 input2 = document.querySelectorAll("input")[2];
+hasil = document.querySelectorAll("input")[0];
+operator = document.querySelectorAll("select")[0];
+btnhasil = document.querySelectorAll("button")[0];
+btnreset = document.querySelectorAll("button")[1];
